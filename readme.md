46da202783cd2a0f76f566bcef0590ec8d1edea8
Wan-Yi Yeh <wyeh2@ncsu.edu>
2018-02-09 14:18:54 -0500

    sample

--- readme.md
+++ readme.md
@@ -7,3 +7,20 @@ Check out the full repo here: (https://github.com/howdyai/botkit-starter-slack)
 
 Also checkout the official Botkit documentation repo here: (https://github.com/howdyai/botkit/blob/master/docs/readme.md#developing-with-botkit)
 
+
+# Resource
+Tutorial vedio: https://www.youtube.com/watch?time_continue=1&v=jTaoU6u1Fcg
+
+botkit middleware(for NLP): https://github.com/howdyai/botkit/blob/master/docs/readme-middlewares.md
+
+
+
+After setup, every time start coding you need to
+
+1. start ngrok(ngrok http 3000) and npm(npm start)
+2. copy "https..." from ngrok
+3. Go to "api.slack.com", click your slack app, find your bot
+4. paste the link + "/oauth" on "OAuth & Permission"
+5. go to "Event Subscription" paste the link + "/slack/receive"
+ 
+when you want to test after change, you should restart npm
--- skills/sample_hears.js
+++ skills/sample_hears.js
@@ -16,5 +16,44 @@ module.exports = function(controller) {
     });
 
 
+    controller.on('direct_message,direct_mention', function(bot, message) {
+        if (matchKeywords(message.text) )
+            bot.reply(message, "sentence contains graduation and subject");
+        else
+            bot.reply(message, "you said " + message.text);
+    });
+
+    controller.hears(['^say (.*)','^say'], 'direct_message,direct_mention', function(bot, message) {
+        if (message.match[1]) {
+             bot.reply(message, "ok, " + message.match[1]);
+        } else {
+            bot.reply(message, 'I will repeat whatever you say.')
+        }
+    });
+
 
 };
+
+var list = ['graduation', 'subject'];
+
+var matchKeywords =  function(message){
+    var strings = message.toLowerCase().split(" ");
+    var containAll = true;
+
+    for (var l = 0; l < list.length; l++){
+        if (!containAll)
+            return false;
+
+        var containThis = false;
+        for (var s = 0; s < strings.length; s++){
+            if (list[l]===strings[s]){
+                containThis = true;
+                break;
+            }
+        }
+
+        if(!containThis)
+            return false;
+    }
+    return true;
+}
